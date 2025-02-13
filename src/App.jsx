43576be70b77de3d383588ad33d1d4e091ba5b70
Victor Ulloa <victoralfonsoulloa@gmail.com>
2025-02-12 20:08:35 -0800

    Add initial assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-            Hello world!
-            </h1>
-        </div>
+        <main>
+            <div className="pattern"/>
+            <div className="wrapper">
+                <header className="header">
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+                </header>
+                <p>Search</p>
+            </div>
+        </main>
     )
 }
 export default App
