60384b3599b9d8d96a2b40718c360274061afa03
Victor Ulloa <victoralfonsoulloa@gmail.com>
2025-02-12 20:50:20 -0800

    Add search bar and hero section to homepage layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Search from "./components/Search.jsx";
+
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState("")
     return (
         <main>
             <div className="pattern"/>
             <div className="wrapper">
-                <header className="header">
+                <header>
+                    <img src="./hero.png" alt="Hero Banner" />
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
                 </header>
-                <p>Search</p>
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
             </div>
         </main>
     )
